9fc221d5d71fdcc7a01d44814aba43d26089bb7b
Ng Guoyou <ng.guoyou@gmail.com>
2016-07-01 16:51:03 +0800

    Implement time travel

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import { Provider} from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import reducer from './reducers';
+import undoable, { excludeAction } from 'redux-undo';
+import { ActionCreators as UndoActionCreators } from 'redux-undo'
 
-export function configureStore(initialState = { game: { entities: [] } }) {
-  const store = createStore(reducer, initialState,
+export function configureStore(initialState = { game: { entities: [], keys: {} } }) {
+  const store = createStore(undoable(reducer, { limit: 120, filter: excludeAction(['KEY_DOWN', 'KEY_UP']) }), initialState,
     window.devToolsExtension && window.devToolsExtension()
   );
   return store;
@@ -13,43 +15,146 @@ export function configureStore(initialState = { game: { entities: [] } }) {
 const store = configureStore();
 
 // Actions
-const addEntity = (position) => {
+const addEntity = (options) => ({
+  type: 'ADD_ENTITY',
+  options
+});
+
+const addBall = (position) => ({
+  type: 'ADD_BALL',
+  position,
+  direction: 0,
+  speed: 10
+});
+
+const moveEntity = (entityId, delta) => ({
+  type: 'MOVE_ENTITY',
+  entityId,
+  delta
+});
+
+const keyChange = (change, code) => {
   return {
-    type: 'ADD_ENTITY',
-    position
+    type: change === 'up' ? 'KEY_UP' : 'KEY_DOWN',
+    key: String.fromCharCode(code).toLowerCase()
   };
+}
+
+const reflect = (entityId) => ({
+  type: 'FLIP_ENTITY_DIRECTION',
+  entityId
+});
+
+const options = {
+  canvas: {
+    width: 800,
+    height: 600
+  },
+  bounds: {
+    x: { lower: 0, upper: 800 },
+    y: { lower: 0, upper: 450 }
+  }
 };
 
 export default class App extends Component {
   tick() {
-    const canvas = document.getElementById('game-canvas');
-    const context = canvas.getContext('2d');
+    const state = store.getState().present.game;
+
+    if (state.keys.e) {
+      store.dispatch(UndoActionCreators.jump(-2));
+      return;
+    };
+
+    for (let i = 0; i < state.entities.length; i++) {
+      if (typeof state.entities[i].tick === 'function') {
+        state.entities[i].tick();
+      }
+    }
+
+    const player = state.entities[0];
+
+    if (state.keys.d) { store.dispatch(moveEntity(0, { x: 10, y: 0 })) }
+    if (state.keys.a) { store.dispatch(moveEntity(0, { x: -10, y: 0 })) }
+    // if (state.keys.w) { store.dispatch(moveEntity(0, { x: 0, y: -10 })) }
+    // if (state.keys.s) { store.dispatch(moveEntity(0, { x: 0, y: 10 })) }
+
+    if (player.position.x < options.bounds.x.lower ||
+        player.position.x > options.bounds.x.upper) {
+      let dX = 0;
+      let dY = 0;
+
+      if (player.position.x < options.bounds.x.lower) {
+        dX = -player.position.x;
+      } else if (player.position.x > options.bounds.x.upper) {
+        dX = options.bounds.x.upper - player.position.x;
+      }
+
+      store.dispatch(moveEntity(0, { x: dX / 10, y: dY}))
+    }
+  }
+
+  draw(canvas, context, store, _delta) {
+    const state = store.getState().present.game;
     context.clearRect(0, 0, canvas.width, canvas.height);
-    const state = store.getState().game;
 
     for (let i = 0; i < state.entities.length; i++) {
       const entity = state.entities[i];
       context.fillRect(
-        entity.position.x - entity.radius,
-        entity.position.y - entity.radius,
-        entity.position.x + entity.radius,
-        entity.position.y + entity.radius
+        entity.position.x - entity.width / 2,
+        entity.position.y - entity.height / 2,
+        entity.width,
+        entity.height
       );
     }
 
-    window.requestAnimationFrame(this.tick.bind(this));
+    window.requestAnimationFrame(this.draw.bind(this, canvas, context, store));
+  }
+
+  bindKeys() {
+    window.addEventListener('keydown', (event) => {
+      store.dispatch(keyChange('down', event.keyCode));
+    });
+
+    window.addEventListener('keyup', (event) => {
+      store.dispatch(keyChange('up', event.keyCode));
+    });
   }
 
   componentDidMount() {
-    store.dispatch(addEntity({ x: 10, y: 10 }));
-    window.requestAnimationFrame(this.tick.bind(this));
+    this.bindKeys();
+    store.dispatch(addEntity({
+      position: { x: options.canvas.width / 2, y: 400 },
+      width: 120
+    }));
+
+    store.dispatch(addEntity({
+      position: { x: options.canvas.width / 2, y: 350 },
+      direction: Math.PI / 4,
+      speed: 1,
+      tick: function() {
+        const player = store.getState().present.game.entities[0];
+        if (this.intersectsWith(player)) {
+          store.dispatch(reflect(this.uid));
+        }
+
+        const dX = Math.cos(this.direction) * this.speed;
+        const dY = Math.sin(this.direction) * this.speed;
+        store.dispatch(moveEntity(this.uid, { x: dX, y: dY }));
+      }
+    }));
+
+    const canvas = document.getElementById('game-canvas');
+    const context = canvas.getContext('2d');
+    const tickrate = 60;
+
+    window.setInterval(this.tick, 1000 / tickrate);
+    window.requestAnimationFrame(this.draw.bind(this, canvas, context, store));
   }
 
   render() {
     return (
       <div>
-        {store.getState().game.entities.length}
-        <canvas id="game-canvas" width={800} height={450}></canvas>
+        <canvas id="game-canvas" width={options.canvas.width} height={options.canvas.height}></canvas>
       </div>
     );
   }
--- src/reducers/game.js
+++ src/reducers/game.js
@@ -1,22 +1,65 @@
+var update = require('react/lib/update');
+
 let totallyUniqueIdCounter = 0;
 
-const makeEntity = (position) => ({
-  uid: totallyUniqueIdCounter++,
-  radius: 10,
-  position
-});
+const makeEntity = (options) => {
+  totallyUniqueIdCounter++;
+  return Object.assign({
+    uid: totallyUniqueIdCounter - 1,
+    width: 20,
+    height: 20,
+    tick: null,
+    intersectsWith: function(other) {
+      return !( this.position.x +  this.width  < other.position.x ||
+               other.position.x + other.width  <  this.position.x ||
+                this.position.y +  this.height < other.position.y ||
+               other.position.y + other.height <  this.position.y)
+    }
+    }, options);
+}
 
-export default function game(state = {}, action) {
-  let newState;
+export default function game(state = { entities: [], keys: {} }, action) {
+  let newState, entity;
 
   switch (action.type) {
     case 'ADD_ENTITY':
       newState = Object.assign({}, state);
-      newState.entities.push(makeEntity(action.position));
+      newState.entities.push(makeEntity(action.options));
       return newState;
     case 'MOVE_ENTITY':
+      entity = state.entities[action.entityId];
+      return update(state, {
+        entities: {
+          [action.entityId]: {
+            position: {
+              x: { $set: entity.position.x + action.delta.x },
+              y: { $set: entity.position.y + action.delta.y }
+            }
+          }
+        }
+      })
+    case 'FLIP_ENTITY_DIRECTION':
+      entity = state.entities[action.entityId];
+      const newDirection = 1.5 * Math.PI + (-1.5 * Math.PI - entity.direction);
+
+      return update(state, {
+        entities: {
+          [action.entityId]: {
+            direction: { $set: newDirection },
+            position: {
+              x: { $set: entity.position.x + Math.cos(newDirection) * entity.speed * 2 },
+              y: { $set: entity.position.y + Math.sin(newDirection) * entity.speed * 2 }
+            }
+          }
+        }
+      })
+    case 'KEY_DOWN':
       newState = Object.assign({}, state);
-      newState.entities[action.entityId].move(action.deltas);
+      newState.keys[action.key] = true;
+      return newState;
+    case 'KEY_UP':
+      newState = Object.assign({}, state);
+      newState.keys[action.key] = false;
       return newState;
     default:
       return state;
