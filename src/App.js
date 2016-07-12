04ed87675e2c9f9a1981a089231af93d73eec321
Ng Guoyou <fauxface+github@gmail.com>
2016-07-13 00:32:42 +0800

    Add blocks, incorrect bouncing algorithm

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ import undoable, { excludeAction } from 'redux-undo';
 import { ActionCreators as UndoActionCreators } from 'redux-undo'
 
 export function configureStore(initialState = { game: { entities: [], keys: {} } }) {
-  const store = createStore(undoable(reducer, { limit: 120, filter: excludeAction(['KEY_DOWN', 'KEY_UP']) }), initialState,
+  const store = createStore(
+    undoable(reducer, {
+      limit: 240,
+      filter: excludeAction(['KEY_DOWN', 'KEY_UP'])
+    }),
+    initialState,
     window.devToolsExtension && window.devToolsExtension()
   );
   return store;
@@ -20,12 +25,13 @@ const addEntity = (options) => ({
   options
 });
 
-const addBall = (position) => ({
-  type: 'ADD_BALL',
-  position,
-  direction: 0,
-  speed: 10
-});
+
+function killEntity(options) {
+  return {
+    type: 'KILL_ENTITY',
+    entityId: options.entityId
+  }
+}
 
 const moveEntity = (entityId, delta) => ({
   type: 'MOVE_ENTITY',
@@ -40,9 +46,16 @@ const keyChange = (change, code) => {
   };
 }
 
-const reflect = (entityId) => ({
+const reflect = (entityId, delta) => ({
   type: 'FLIP_ENTITY_DIRECTION',
-  entityId
+  entityId,
+  delta
+});
+
+const setEntityPosition = (entityId, position) => ({
+  type: 'SET_ENTITY_POSITION',
+  entityId,
+  position
 });
 
 const options = {
@@ -60,13 +73,15 @@ export default class App extends Component {
   tick() {
     const state = store.getState().present.game;
 
+    // document.querySelector('#debug').textContent = JSON.stringify(state);
+
     if (state.keys.e) {
       store.dispatch(UndoActionCreators.jump(-2));
       return;
     };
 
     for (let i = 0; i < state.entities.length; i++) {
-      if (typeof state.entities[i].tick === 'function') {
+      if (state.entities[i] !== null && typeof state.entities[i].tick === 'function') {
         state.entities[i].tick();
       }
     }
@@ -75,33 +90,42 @@ export default class App extends Component {
 
     if (state.keys.d) { store.dispatch(moveEntity(0, { x: 10, y: 0 })) }
     if (state.keys.a) { store.dispatch(moveEntity(0, { x: -10, y: 0 })) }
-    // if (state.keys.w) { store.dispatch(moveEntity(0, { x: 0, y: -10 })) }
-    // if (state.keys.s) { store.dispatch(moveEntity(0, { x: 0, y: 10 })) }
 
     if (player.position.x < options.bounds.x.lower ||
-        player.position.x > options.bounds.x.upper) {
+        player.position.x > options.bounds.x.upper - player.width) {
       let dX = 0;
       let dY = 0;
 
       if (player.position.x < options.bounds.x.lower) {
         dX = -player.position.x;
-      } else if (player.position.x > options.bounds.x.upper) {
-        dX = options.bounds.x.upper - player.position.x;
+      } else if (player.position.x > options.bounds.x.upper - player.width) {
+        dX = options.bounds.x.upper - player.position.x - player.width;
       }
 
       store.dispatch(moveEntity(0, { x: dX / 10, y: dY}))
     }
   }
 
+  cameraShake = { x: 0, y: 0, reduction: 0.95 };
+
   draw(canvas, context, store, _delta) {
     const state = store.getState().present.game;
-    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    if (store.getState().present.game.keys.e) {
+      context.fillStyle = 'rgba(255, 255, 255, 0.01)'; // Trails
+      context.fillRect(0, 0, canvas.width, canvas.height);
+    } else {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+    }
 
     for (let i = 0; i < state.entities.length; i++) {
       const entity = state.entities[i];
+      if (entity === null) continue;
+
+      context.fillStyle = entity.color || '#000';
       context.fillRect(
-        entity.position.x - entity.width / 2,
-        entity.position.y - entity.height / 2,
+        entity.position.x,
+        entity.position.y,
         entity.width,
         entity.height
       );
@@ -122,18 +146,28 @@ export default class App extends Component {
 
   componentDidMount() {
     this.bindKeys();
+    // Paddle, hardcoded
     store.dispatch(addEntity({
       position: { x: options.canvas.width / 2, y: 400 },
       width: 120
     }));
 
+    // Ball, hardcoded
     store.dispatch(addEntity({
       position: { x: options.canvas.width / 2, y: 350 },
       direction: Math.PI / 4,
-      speed: 1,
+      speed: 5,
+      color: 'rgba(255, 0, 0, 1)',
       tick: function() {
-        const player = store.getState().present.game.entities[0];
-        if (this.intersectsWith(player)) {
+        const state = store.getState().present.game;
+        const player = state.entities[0];
+
+        if (this.intersectsWith(player) ||
+            this.outOfBounds(0, options.canvas.width - 30, 0, options.canvas.height - 30)) {
+          let delta = 0;
+          if (state.keys.d) { delta = 2 * Math.PI }
+          if (state.keys.a) { delta = Math.PI }
+
           store.dispatch(reflect(this.uid));
         }
 
@@ -142,6 +176,26 @@ export default class App extends Component {
         store.dispatch(moveEntity(this.uid, { x: dX, y: dY }));
       }
     }));
+
+    // Blocks
+    for (let i = 0; i < 12; i++) {
+      for (let j = 0; j < 6; j++) {
+        const x = 155 + i * 44;
+        const y = 50 + j * 22;
+        store.dispatch(addEntity({
+          position: { x, y },
+          width: 40,
+          color: `rgba(${x % 255}, ${y % 255}, ${(x + y) % 255}, 1)`,
+          tick: function() {
+            const ball = store.getState().present.game.entities[1];
+            if (this.intersectsWith(ball)) {
+              store.dispatch(killEntity({ entityId: this.uid }));
+              store.dispatch(reflect(1)); // reflect ball
+            }
+          }
+        }));
+      }
+    }
 
     const canvas = document.getElementById('game-canvas');
     const context = canvas.getContext('2d');
@@ -154,7 +208,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <canvas id="game-canvas" width={options.canvas.width} height={options.canvas.height}></canvas>
+        <canvas id="game-canvas" width={options.canvas.width} height={options.canvas.height} style={{ border: 'solid 1px blue'}}></canvas>
       </div>
     );
   }
--- src/reducers/game.js
+++ src/reducers/game.js
@@ -14,18 +14,36 @@ const makeEntity = (options) => {
                other.position.x + other.width  <  this.position.x ||
                 this.position.y +  this.height < other.position.y ||
                other.position.y + other.height <  this.position.y)
+    },
+    outOfBounds: function(minX, maxX, minY, maxY) {
+      return this.position.x < minX || this.position.x > maxX ||
+             this.position.y < minY || this.position.y > maxY
     }
-    }, options);
+  }, options);
 }
+
+let deg2rad = (deg) => deg * 180 / Math.PI;
 
 export default function game(state = { entities: [], keys: {} }, action) {
   let newState, entity;
 
   switch (action.type) {
     case 'ADD_ENTITY':
-      newState = Object.assign({}, state);
-      newState.entities.push(makeEntity(action.options));
-      return newState;
+      return update(state, {
+        entities: {
+          $push: [makeEntity(action.options)]
+        }
+      })
+      // Don't use Object.assign, it only shallow updates
+      // newState = Object.assign({}, state);
+      // newState.entities.push(makeEntity(action.options));
+      // return newState;
+    case 'KILL_ENTITY':
+      return update(state, {
+        entities: {
+          [action.entityId]: { $set: null }
+        }
+      })
     case 'MOVE_ENTITY':
       entity = state.entities[action.entityId];
       return update(state, {
@@ -38,17 +56,40 @@ export default function game(state = { entities: [], keys: {} }, action) {
           }
         }
       })
+    case 'SET_ENTITY_POSITION':
+      entity = state.entities[action.entityId];
+      return update(state, {
+        entities: {
+          [action.entityId]: {
+            position: {
+              x: { $set: action.position.x },
+              y: { $set: action.position.y }
+            }
+          }
+        }
+      })
     case 'FLIP_ENTITY_DIRECTION':
       entity = state.entities[action.entityId];
-      const newDirection = 1.5 * Math.PI + (-1.5 * Math.PI - entity.direction);
+
+      let newDirection;
+      const delta = action.delta || entity.direction;
+
+      const direction = entity.direction;
+      if (entity.direction > Math.PI && entity.direction < Math.PI * 2) {
+        newDirection = (entity.direction + delta) / 2 + (Math.PI * 3/ 2);
+      } else {
+        newDirection = (entity.direction + delta) / 2 - (Math.PI / 2);
+      }
+
+      // newDirection = newDirection % (2 * Math.PI)
 
       return update(state, {
         entities: {
           [action.entityId]: {
             direction: { $set: newDirection },
             position: {
-              x: { $set: entity.position.x + Math.cos(newDirection) * entity.speed * 2 },
-              y: { $set: entity.position.y + Math.sin(newDirection) * entity.speed * 2 }
+              x: { $set: entity.position.x + Math.cos(newDirection) * 16 },
+              y: { $set: entity.position.y + Math.sin(newDirection) * 16 }
             }
           }
         }
